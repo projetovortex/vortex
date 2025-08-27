9d9252758c7d5c2b31e3321f4e9342f6c89d4669
gatoarrupiada <gatoarrupiada@gmail.com>
2025-08-27 01:40:55 +0000

    header e home adaptado para mobile e seu modo escuro

--- src/components/HeaderLogadoClaro.jsx
+++ src/components/HeaderLogadoClaro.jsx
@@ -1,6 +1,8 @@
 import iconVortex from "../assets/iconVortex.png"
 import modoClaro from "../assets/modoClaro.png"
 import modoEscuro from "../assets/modoEscuro.png"
+import menu from "../assets/menu.png"
+import menuEscuro from "../assets/menuEscuro.png"
 import { Link } from "react-router-dom"
 import React, { useEffect } from "react"
 import { useState } from "react"
@@ -18,16 +20,21 @@ export function HeaderLogadoClaro() {
         }
     }, [theme])
 
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return(
         <div>
-            <header className="px-10 py-5 pb-25 flex justify-between align-middle h-25 font-hanken font-bold text-white text-2xl">
+            <header className="px-10 py-5 lg:pb-25 flex justify-between align-middle h-25 font-hanken font-bold text-white text-2xl">
 
                 <div className="flex align-center justify-center gap-3">
-                    <Link to={"/"} className="cursor-pointer w-fit"><img className="w-20 h-20" src={iconVortex} alt="icone vortex"/></Link>
-                    <Link to={"/"} className="cursor-pointer pt-6">VORTEX</Link>
+                    <Link to={"/"} className="cursor-pointer w-fit"><img className="lg:w-20 lg:h-20 w-15 h-15" src={iconVortex} alt="icone vortex"/></Link>
+                    <Link to={"/"} className="lg:text-white text-purple-mobile dark:text-white cursor-pointer lg:pt-6 pt-4">VORTEX</Link>
                 </div>
                 
-                <div className="flex gap-35 pt-6">
+                <div className="lg:flex hidden gap-35 pt-6">
                     <Link to={"/Conheca"}>Conheça</Link>
                     <Link to={"/Jogo"}>Jogo</Link>
                     <Link to={"/Aplicativo"}>App</Link>
@@ -35,13 +42,36 @@ export function HeaderLogadoClaro() {
                 </div>
                 
                 <div>
-                    <button className="cursor-pointer" onClick={() => setTheme(prevTheme => prevTheme === "dark" ? "" : "dark")}>
+                    <button className="lg:block hidden cursor-pointer" onClick={() => setTheme(prevTheme => prevTheme === "dark" ? "" : "dark")}>
                         {theme === "dark" ? (
                             <img className="w-20 h-20" src={modoEscuro} alt="icone modo escuro"/>
                         ) : (
                             <img className="w-20 h-20" src={modoClaro} alt="icone modo claro"/>
                         )}
                     </button>
+
+                    <button className="cursor-pointer pt-5 dark:hidden" onClick={toggleMenu} aria-controls="mobile-menu" aria-expanded={isOpen}>
+                        <img className="lg:hidden w-10 h-10" src={menu} alt="menu" />
+                    </button>
+                    <button className="hidden dark:block cursor-pointer pt-5" onClick={toggleMenu} aria-controls="mobile-menu" aria-expanded={isOpen}>
+                        <img className="lg:hidden w-10 h-10" src={menuEscuro} alt="menu" />
+                    </button>
+
+                    <div className={`${isOpen ? 'block' : 'hidden'} absolute right-0 z-10 h-screen p-10 w-50vh mt-2 bg-cyan-mobile dark:bg-dark-purple rounded-md shadow-lg lg:hidden`} id="mobile-menu">
+                        <div className="text-purple-mobile dark:text-white flex flex-col gap-10" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                            <Link to={"/Conheca"}>Conheça</Link>
+                            <Link to={"/Jogo"}>Jogo</Link>
+                            <Link to={"/Aplicativo"}>App</Link>
+                            <Link to={"/SobreNos"}>Sobre nós</Link>
+                            <button className="cursor-pointer" onClick={() => setTheme(prevTheme => prevTheme === "dark" ? "" : "dark")}>
+                                {theme === "dark" ? (
+                                    <img className="w-20 h-20" src={modoEscuro} alt="icone modo escuro"/>
+                                ) : (
+                                    <img className="w-20 h-20" src={modoClaro} alt="icone modo claro"/>
+                                )}
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </header>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,15 @@ import { HeaderLogadoClaro } from "../components/HeaderLogadoClaro"
 
 export function Home() {
     return(
-        <div className="bg-[url('./src/assets/bgClaro.png')] dark:bg-[url('./src/assets/bgEscuro.png')] bg-cover min-h-screen">
+        <div className="lg:bg-[url('./src/assets/bgClaro.png')] lg:dark:bg-[url('./src/assets/bgEscuro.png')] bg-cyan-mobile dark:bg-dark-purple bg-cover min-h-screen">
             <HeaderLogadoClaro/>
-            <div className="flex justify-center itens-center pt-20 gap-20 font-questrial text-white">
-                <div className="pt-30 pr-20">
-                    <h1 className="text-9xl text-light-blue pl-15">VORTEX</h1>
-                    <h2 className="text-7xl">Gere energia a partir <br />de seu movimento</h2>
+            <div className="min-h-screen lg:bg-none bg-cover bg-[url('./src/assets/ciclistaMobile.png')] flex justify-center itens-center pt-20 pb-10 gap-20 font-questrial text-white">
+                <div className="lg:block flex flex-col justify-center items-center xl:pt-30 xl:pr-20">
+                    <h1 className="lg:text-shadow-none text-shadow-sm text-shadow-cyan-shadow-como text-8xl lg:text-9xl text-white lg:text-light-blue xl:pl-15">VORTEX</h1>
+                    <h2 className="lg:text-shadow-none text-shadow-sm text-shadow-cyan-shadow-como lg:text-7xl text-4xl">Gere energia a partir <br />de seu movimento</h2>
                 </div>
                 
-                <img className="" src={ciclista} alt="ilustração ciclista" />
+                <img className="lg:block w-120 hidden" src={ciclista} alt="ilustração ciclista" />
             </div>
         </div>
     )
