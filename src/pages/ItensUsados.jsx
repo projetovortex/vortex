a47490ebb31844f0ad8e3bed537455f8972840f1
gatoarrupiada <gatoarrupiada@gmail.com>
2025-08-28 16:33:27 +0000

    itens carrossel funcionando

--- src/pages/ItensUsados.jsx
+++ src/pages/ItensUsados.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { HeaderLogadoClaro } from "../components/HeaderLogadoClaro"
 import quadrado from "../assets/quadrado.png"
 import { CardEsp32 } from "../components/CardEsp32"
@@ -12,20 +13,45 @@ import { CardDinamo } from "../components/CardDinamo"
 import setaEsquerda from "../assets/setaEsquerda.png"
 import setaDireita from "../assets/setaDireita.png"
 
+const cards = [
+    <CardEsp32 key="esp32" />,
+    <CardBuckBoost key="buckboost" />,
+    <CardCapacitor key="capacitor" />,
+    <CardJumpers key="jumpers" />,
+    <CardBateria key="bateria" />,
+    <CardSensor key="sensor" />,
+    <CardModulo key="modulo" />,
+    <CardStepUp key="stepup" />,
+    <CardDinamo key="dinamo" />,
+  ];
+
 export function ItensUsados() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length);
+    };
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? cards.length - 1 : prevIndex - 1
+        );
+    };
+
     return(
         <div className="bg-[url('./src/assets/bgJogo1.png')] dark:bg-[url('./src/assets/bgJogoEscuro.png')] bg-cover">
             <HeaderLogadoClaro/>
             <div className="h-screen bg-white dark:bg-gray-200 flex flex-col justify-center items-center">
-                <h1 className="lg:text-7xl text-5xl text-center text-purple-button font-questrial pb-5 lg:pt-10 text-shadow-sm text-shadow-cyan-shadow-white">Itens Usados</h1>
+                <h1 className="lg:text-7xl text-5xl text-center text-purple-button font-questrial pb-5 text-shadow-sm text-shadow-cyan-shadow-white">Itens Usados</h1>
 
                 <div className="flex">
                     <button className="cursor-pointer w-100 h-100">
-                        <img src={setaEsquerda} alt="seta voltar" />
+                        <img onClick={handlePrev} src={setaEsquerda} alt="seta voltar" />
                     </button>
-                    <CardStepUp/>
+                    <div className='w-250 h-100'>
+                    {cards[currentIndex]}
+                    </div>
+                    
                     <button className="cursor-pointer w-100 h-100">
-                        <img src={setaDireita} alt="seta avançar" />
+                        <img onClick={handleNext} src={setaDireita} alt="seta avançar" />
                     </button>
                 </div>
             </div>
