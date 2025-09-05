6a47bce95874f4a52fbbdabb7c12936008204cfc
gatoarrupiada <gatoarrupiada@gmail.com>
2025-09-05 15:38:40 +0000

    ajuste fundo mobile home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export function Home() {
     return(
         <div className={`lg:bg-[url('/src/assets/bgClaro.png')] lg:dark:bg-[url('/src/assets/bgEscuro.png')] bg-cyan-mobile dark:bg-dark-purple bg-cover min-h-screen`}>
             <HeaderLogadoClaro/>
-            <div className="lg:bg-none bg-cover bg-[url('./src/assets/ciclistaMobile.png')] flex justify-center itens-center lg:px-10 pt-20 pb-10 gap-20 font-questrial text-white">
+            <div className="lg:bg-none lg:min-h-fit min-h-screen bg-cover bg-[url('/src/assets/ciclistaMobile.png')] flex justify-center itens-center lg:px-10 pt-20 pb-10 gap-20 font-questrial text-white">
                 <div className="lg:block flex flex-col justify-center items-center xl:pt-30 xl:pr-20">
                     <h1 className="lg:text-shadow-none text-shadow-sm text-shadow-cyan-shadow-como text-7xl lg:text-9xl text-white lg:text-light-blue xl:pl-15">VORTEX</h1>
                     <h2 className="lg:text-shadow-none text-shadow-sm text-shadow-cyan-shadow-como lg:text-7xl text-3xl">Gere energia a partir <br />de seu movimento</h2>
