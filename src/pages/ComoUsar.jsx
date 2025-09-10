cc5d568aea1e1d94991bf9dbf88c45d433884c7d
gatoarrupiada <gatoarrupiada@gmail.com>
2025-09-10 17:53:01 +0000

    texto de instruções adicionado Márcio pega ai pro senhor

--- src/pages/ComoUsar.jsx
+++ src/pages/ComoUsar.jsx
@@ -7,11 +7,13 @@ export function ComoUsar() {
             <HeaderLogadoClaro/>
             <div className="flex flex-col justify-center items-center lg:pb-10 lg:p-10 font-questrial">
                 <h1 className="lg:text-7xl text-5xl text-center lg:text-very-dark-blue text-purple-mobile-conheca pb-5 text-shadow-sm text-shadow-cyan-shadow-como dark:text-light-blue">Como usar o Vortex?</h1>
+                <div className="flex items-center lg:gap-20 lg:pt-20 ">
+                    <p className="lg:block hidden w-140 text-center text-white text-4xl">Para utilizar nosso projeto, basta pedalar a bicicleta que o Vortex já gera a energia para o carregamento de seu dispositivo.
+                    A entrada USB para o carregamento está na caixa embaixo do banco.</p>
                 <img className="lg:p-0 p-10" src={imagemProjeto} alt="" />
-                <p className="lg:hidden block px-10 lg:text-white dark:text-cyan-button text-purple-mobile-conheca text-2xl pb-10">Projeto que converte energia do
-                        movimento em eletricidade para
-                        carregar pequenos dispositivos
-                        em situações emergenciais.</p>
+                </div>
+                <p className="lg:hidden block px-10 lg:text-white dark:text-cyan-button text-purple-mobile-conheca text-2xl pb-10">Para utilizar nosso projeto, basta pedalar a bicicleta que o Vortex já gera a energia para o carregamento de seu dispositivo.
+                A entrada USB para o carregamento está na caixa embaixo do banco.</p>
             </div>
         </div>
     )
